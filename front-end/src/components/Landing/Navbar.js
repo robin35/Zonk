6f54519eeeae00a872e48dd0b88214127422e383
serena mallavarapu <serena.mallavarapu@gmail.com>
2019-08-06 20:59:22 -0500

    took out extra tabs on navbar

--- front-end/src/components/Landing/Navbar.js
+++ front-end/src/components/Landing/Navbar.js
@@ -4,16 +4,15 @@ import { Link } from 'react-router-dom';
 const Navbar = props => {
     return (
         <header>
-            <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+            <nav className='navbar navbar-expand-lg navbar-light bg-light'>
                 <div></div>
                 <div><Link to='/' className='navbar-brand'>Zonk!</Link></div>
                                 
                 <div className='navbar-nav'>
                     <Link to='/' className='nav-item nav-link'>Home</Link>
                     <Link to='/playgame' className='nav-item nav-link'>Play Game</Link>
-                    <Link to='/addquestions' className='nav-item nav-link'>Add Questions</Link>
-                    <Link to='/login' className='nav-item nav-link'>Sign In</Link>
-                    <Link to='/join' className='nav-item nav-link'>Sign Up</Link>
+                    <Link to='/addquestions' className='nav-item nav-link'>Create Questions</Link>
+
                 </div>
 
             </nav>
